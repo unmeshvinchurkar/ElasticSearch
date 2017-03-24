83a2551e3503e7b5de32b2b731bee4cfa6e8890e
Unmesh Vinchurkar <unmeshvinchurkar@fico.com>
2017-03-24 15:09:21 +0530

    Removed unused references.

--- src/main/java/com/blaze/search/Main2.java
+++ src/main/java/com/blaze/search/Main2.java
@@ -1,38 +1,10 @@
 package com.blaze.search;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.elasticsearch.common.unit.Fuzziness;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
-import org.elasticsearch.index.query.QueryStringQueryBuilder.Operator;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-
-import com.blaze.parser.EntityLoader;
 import com.blaze.parser.InstanceFileContentParser;
 import com.blaze.parser.InstanceFileLoader;
-import com.blaze.search.elasticsearch.DocumentManager;
-import com.blaze.search.elasticsearch.ESIndexManager;
-import com.blaze.search.elasticsearch.JestClientBuilderFactory;
-
-import io.searchbox.client.JestClient;
-import io.searchbox.client.JestResult;
-import io.searchbox.core.Search;
-import io.searchbox.core.SearchResult;
-import io.searchbox.core.SearchResult.Hit;
 import net.sf.json.JSONObject;
 
 public class Main2 {
